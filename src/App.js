1afae4aa150c6e5152426e17a090026834b82905
The Dung <dungthemai20@gmail.com>
2022-09-28 20:31:18 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Mai The Dung</p>
+        <p>Mai Thế Dũng</p>
       </header>
     </div>
   );
